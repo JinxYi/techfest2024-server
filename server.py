672dd90aa195643223352eee3caca8123a43a6d8
diipperss <dibya6665@gmail.com>
2024-02-06 04:17:25 +0800

    final UI draft

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import os
+
 from flask import Flask, jsonify, request, render_template
+import google.generativeai as palm
 from flask_mysqldb import MySQL  # Added import for MySQL
 
 from transformers import T5ForConditionalGeneration,T5Tokenizer
@@ -16,6 +18,8 @@ import en_core_web_sm
 nlp = en_core_web_sm.load()
 
 
+
+palm.configure(api_key='AIzaSyBcbCy0PNsrzB-txCnN7YU5OnSb_4FbIwA')
 template_dir = os.path.abspath('./template')
 test_config = None
 
@@ -55,6 +59,28 @@ try:
 except OSError:
     pass
 
+@app.route('/chatbot')
+def chatbot():
+    return render_template('chatbot.html')
+@app.route('/send_message',methods=['POST'])
+def send_message():
+    user_input = request.form.get('user_input')
+
+    if user_input == '0':
+        return jsonify({'response': 'Goodbye!'})
+
+    completion = palm.generate_text(
+        model="models/text-bison-001",
+        prompt=user_input,
+        temperature=0,
+        max_output_tokens=200,
+    )
+
+    bot_response = completion.result
+    return jsonify({'response': bot_response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 @app.route('/flashcards',)
 def get_flashcard():
     # Insert flashcard into the database
@@ -65,19 +91,9 @@ def get_flashcard():
 
     # Convert the result to a list of dictionaries for JSON response
     flashcards_list = [{'id': card[0], 'question': card[1], 'answer': card[2]} for card in flashcards]
-    print("flashcards_list", flashcards_list)
-    return jsonify({'flashcards': flashcards_list})
+    return render_template('flashcards.html',flashcards_list=flashcards_list)
 
-# initlilizing summary model
-summary_model = T5ForConditionalGeneration.from_pretrained('t5-base')
-summary_tokenizer = T5Tokenizer.from_pretrained('t5-base')
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-summary_model = summary_model.to(device)
 
-# initializing flashcard model
-question_model = T5ForConditionalGeneration.from_pretrained('ramsrigouthamg/t5_squad_v1')
-question_tokenizer = T5Tokenizer.from_pretrained('ramsrigouthamg/t5_squad_v1')
-question_model = question_model.to(device)
 @app.route('/summarize', methods=['POST'])
 def summarize():
     if request.method == 'POST':
@@ -114,6 +130,6 @@ def summarize():
             mysql.connection.commit()
 
         cursor.close()
-        return render_template('hackathon.html', out=out)
+        return render_template('hackathon.html', summarizer_result=out)
 
 
