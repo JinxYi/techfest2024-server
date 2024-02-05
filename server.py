9cf5568135a2c0bd16048fcbfca6483f26ceb7b0
JinxYi <jingyi0939@gmail.com>
2024-02-05 22:54:47 +0800

    integrated summarizer with

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
 import os
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
 
+
+from transformers import pipeline
 template_dir = os.path.abspath('./template')
-
 test_config = None
-    # create and configure the app
+
+MAX_LENGTH = 130
+
+## set up summarizer model
+# using pipeline API for summarization task
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+
+## server code
+test_config = None
+# create and configure the app
 app = Flask(__name__, instance_relative_config=True, template_folder=template_dir)
+app.config['DEBUG'] = True
 app.config.from_mapping(
     SECRET_KEY='HAHAHI',
     static_folder='static',
@@ -31,10 +42,13 @@ try:
 except OSError:
     pass
 
-# a simple page that says hello
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    if request.method == 'POST':
+        user_input = request.form.get('notes')
+        max_length = int(request.form.get('word_count'))
+        summarizer_output = summarizer(user_input, max_length=max_length, min_length=30, do_sample=False)
+        return summarizer_output
 
 
 
